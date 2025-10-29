c9be2c43b66d938794be84d12d49fa60fce442f9
Pablo <pablo.vaz.111@gmail.com>
2025-10-29 12:22:38 +0100

    Llegan los Ents a Java

--- Hola.java
+++ Hola.java
@@ -1,5 +1,6 @@
 class Hola {
  public static void main(String[] args){
  System.out.println("Welcome to the Java World");
+ System.out.println("I have no more branches to commit, said the Ent");
  }
 }
